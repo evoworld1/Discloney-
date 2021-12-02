31eb675dbfd00e860a011dbffb81cbdb8f6349bf
evoworld1 <evoworld1@gmail.com>
2021-12-02 23:27:59 +0100

    finish styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,7 @@ const Header = (props) => {
           <span>series</span>
         </a>
       </NavMenu>
+      <LoginBtn>login</LoginBtn>
     </Nav>
   );
 };
@@ -123,6 +124,22 @@ const NavMenu = styled.div`
 
   @media (max-width: 768px) {
     display: none;
+  }
+`;
+
+const LoginBtn = styled.a`
+  background-color: rgba(0, 0, 0, 0.65);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 5px;
+  transition: all 200ms ease-out 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: rgba(0, 0, 0, 0.65);
+    border-color: transparent;
   }
 `;
 export default Header;
